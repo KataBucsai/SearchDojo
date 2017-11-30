25f31f99e72005c4f0c0862538235585d5a5a33a
KataBucsai <kata.kovacs@gmail.com>
2017-11-30 09:19:23 +0100

    create layout

--- SearchDojo/Form1.Designer.cs
+++ SearchDojo/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.labelText = new System.Windows.Forms.Label();
+            this.labelMatched = new System.Windows.Forms.Label();
+            this.labelPattern = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(350, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 179);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 49);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 179);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(92, 302);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(350, 302);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(24, 30);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(28, 13);
+            this.labelText.TabIndex = 4;
+            this.labelText.Text = "Text";
+            // 
+            // labelMatched
+            // 
+            this.labelMatched.AutoSize = true;
+            this.labelMatched.Location = new System.Drawing.Point(347, 30);
+            this.labelMatched.Name = "labelMatched";
+            this.labelMatched.Size = new System.Drawing.Size(49, 13);
+            this.labelMatched.TabIndex = 5;
+            this.labelMatched.Text = "Matched";
+            // 
+            // labelPattern
+            // 
+            this.labelPattern.AutoSize = true;
+            this.labelPattern.Location = new System.Drawing.Point(36, 307);
+            this.labelPattern.Name = "labelPattern";
+            this.labelPattern.Size = new System.Drawing.Size(41, 13);
+            this.labelPattern.TabIndex = 6;
+            this.labelPattern.Text = "Pattern";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 360);
+            this.Controls.Add(this.labelPattern);
+            this.Controls.Add(this.labelMatched);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Label labelMatched;
+        private System.Windows.Forms.Label labelPattern;
     }
 }
 
